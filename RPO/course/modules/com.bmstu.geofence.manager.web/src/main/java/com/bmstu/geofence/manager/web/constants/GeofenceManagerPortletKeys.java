6f84fa0feba8cd20fb2346c17e81c0b21233e79a
nganguen <nga.nguen@mail.ru>
2018-03-07 19:43:51 +0300

    RPO

---
+++ RPO/course/modules/com.bmstu.geofence.manager.web/src/main/java/com/bmstu/geofence/manager/web/constants/GeofenceManagerPortletKeys.java
@@ -0,0 +1,10 @@
+package com.bmstu.geofence.manager.web.constants;
+
+/**
+ * @author Laptop_Viet
+ */
+public class GeofenceManagerPortletKeys {
+
+	public static final String GeofenceManager = "GeofenceManager";
+
+}
